9bf3390a5733d37c8450514cfad243f1f9d749cf
marplatense <mariano.mara@gmail.com>
2011-04-03 20:32:00 -0300

    error while packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name = "python-linkedin",
-      version = "1.6.1",
+      version = "1.6.2",
       description = "Python Interface for the LinkedIn API",
       author = "Ozgur Vatansever",
       license = open("LICENSE").read(),
